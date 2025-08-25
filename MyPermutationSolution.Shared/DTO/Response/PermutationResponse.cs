49b9cda7e78a003d48738d26e6f3cee1195aa066
yaiselrodriguez <yaisel.rodriguez@ntsprint.com>
2025-08-25 19:12:26 -0400

    Add the service with the method to calculate the permutation Add the interface and configure the programs Add the Controller to the api Add the response and requests class for the data

---
+++ MyPermutationSolution.Shared/DTO/Response/PermutationResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPermutationSolution.Shared.DTO.Response
+{
+    public class PermutationResponse
+    {
+        public int[] RequestData { get; set; } = Array.Empty<int>();
+        public int[] ResponseData { get; set; } = Array.Empty<int>();
+        public DateTime CalculatedDate { get; set; }
+        public required string Message { get; set; }
+    }
+}
