9afdee8bb95703d2de5d7fb10cd1bee453b69907
yaiselrodriguez <yaisel.rodriguez@ntsprint.com>
2025-08-25 19:47:57 -0400

    Add the Unit Test project

---
+++ MyPermutationSolution.Test/PermutationServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using MyPermutationSolution.Server.Services;
+using MyPermutationSolution.Shared.DTO.Request;
+
+namespace MyPermutationSolution.Test
+{
+    public class PermutationServiceTests
+    {
+        private readonly Mock<ILogger<PermutationService>> _loggerMock;
+        private readonly PermutationService _service;
+
+        public PermutationServiceTests()
+        {
+            _loggerMock = new Mock<ILogger<PermutationService>>();
+            _service = new PermutationService(_loggerMock.Object);
+        }
+
+        [Fact]
+        public void CalculatePermutation_WithSingleElement_ReturnsSameArray()
+        {
+            var request = new PermutationRequest { Vector = new[] { 1, 2, 3 } };
+
+            var response = _service.CalculatePermutation(request);
+
+            Assert.Equal(new[] { 1, 2, 3 }, response.RequestData);
+            Assert.Equal(new[] { 1, 3, 2 }, response.ResponseData);
+            Assert.Equal("The permutation was calculated successfully", response.Message);
+        }
+        [Fact]
+        public void CalculateNextPermutation_WithInvalidArray_ThrowsArgumentException()
+        {
+            // Arrange
+            int[] invalidArray = new int[101]; // Demasiado largo
+            var request = new PermutationRequest { Vector = invalidArray };
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => _service.CalculatePermutation(request));
+        }
+
+    }
+}
