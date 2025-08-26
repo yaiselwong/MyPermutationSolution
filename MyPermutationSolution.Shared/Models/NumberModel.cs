42b5e3a2182a52d654098da0a84f291f5bb0a7c9
yaiselwong <yaisel.rodriguez@ntsprint.com>
2025-08-26 08:51:45 -0400

    Add the form to enter the numbers Add model to control the restrictions for the vector

---
+++ MyPermutationSolution.Shared/Models/NumberModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPermutationSolution.Shared.Models
+{
+    public class NumberModel
+    {
+        [Required(ErrorMessage = "A number is mandatory")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Only numbers are avaliable")]
+        [Range(1, 100, ErrorMessage = "The number must be beetween 1 y 100")]
+        public string NumberAdded { get; set; } = string.Empty;
+    }
+}
