e29f19c281dc0f20ad90a64ba5d97883f61e8d6a
yaiselrodriguez <yaisel.rodriguez@ntsprint.com>
2025-08-26 17:49:19 -0400

    Add Errorboundary component Add httphandler to intercept http errors

---
+++ MyPermutationSolution.Client/Services/HttpErrorHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace MyPermutationSolution.Client.Services
+{
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class HttpErrorHandler : DelegatingHandler
+    {
+        public HttpErrorHandler(HttpMessageHandler innerHandler) : base(innerHandler) { }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = await base.SendAsync(request, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                // Aquí puedes lanzar excepción para que la capture ErrorBoundary
+                var content = await response.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Error {(int)response.StatusCode}: {response.ReasonPhrase} - {content}");
+            }
+
+            return response;
+        }
+    }
+}
